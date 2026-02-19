c8b21e11d2c44a564fcb796306ab702939989bb2
hugh <hugh@vaire.co>
2026-02-19 02:04:30 -0600

    add mathlib and some basic definitions

--- DsrtLean/Basic.lean
+++ DsrtLean/Basic.lean
@@ -1,1 +1,31 @@
-def hello := "world"
+import Mathlib.Data.Finset.Basic
+
+structure Node where
+  id: Nat
+  deriving Repr, BEq, Hashable, Ord
+
+inductive TransistorType where
+  | nmos
+  | pmos
+  deriving Repr, BEq, Hashable
+
+structure Transistor (nodes: Finset Node) where -- argument enforces that all referenced nodes are part of a given set
+  source: nodes
+  gate: nodes
+  drain : nodes
+  type: TransistorType
+  deriving Repr, BEq, Hashable
+
+structure Net where
+  nodes: Finset Node
+  transistors : Finset (Transistor nodes)
+
+inductive NodeState where
+  | high
+  | weak_high
+  | weak_low
+  | unknown
+
+structure State (nodes: Finset Node) where
+  map : nodes -> NodeState
+  powered : nodes -> Bool
