a362aa4a24fefe1e52516e8b87b7a2037376cab9
hugh <hugh@vaire.co>
2026-02-19 13:15:34 -0600

    update definitions to be cleaner; add comments describing intended proofs

--- DsrtLean/Basic.lean
+++ DsrtLean/Basic.lean
@@ -26,6 +26,32 @@ inductive NodeState where
   | weak_low
   | unknown
 
-structure State (nodes: Finset Node) where
-  map : nodes -> NodeState
-  powered : nodes -> Bool
+structure State (net: Net) where
+  map : net.nodes -> NodeState
+  powered : net.nodes -> Bool
+
+-- define connectivity in a state
+
+-- define CON, REV0, REV1, STAT
+
+-- define CAP, DDC
+
+-- show CON ∧ REV0 ∧ REV1 -> CAP
+
+-- show CON ∧ REV0 ∧ REV1 -> DDC
+
+-- show CAP ∧ REV0 ∧ REV1 -> CON
+
+-- show uniqueness: if A, B and A, C both satisfy DDC and CAP, then B = C
+
+-- define dsrt_sim(A, powered) as a function implementing the simulation algorithm
+
+-- show if algo doesn't output error, output matches REV0, REV1, DDC, CAP
+  -- deduce that therefore output matches CON, as REV0 ∧ REV1 ∧ CAP -> CON
+
+-- show if algo outputs short-circuit, correct assignment is impossible
+
+-- show if algo outputs REV0 or REV1 error, correct assignment is impossible
+  -- relies on uniqueness given we've already generated DDC and CAP compliant assignment when we output REV0 or REV1 error
+
+-- bonus: show algorithm is reversible
